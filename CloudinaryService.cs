f2dd3a0761dbce0d2dfe01a28321098414deda88
Yolchu <yolchunasib@gmail.com>
2020-02-12 16:12:44 +0400

    Create CloudinaryService.cs

---
+++ CloudinaryService.cs
@@ -0,0 +1,54 @@
+using System.IO;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Core.Services.Rest;
+using Microsoft.Extensions.Configuration;
+
+namespace Services.Rest
+{
+    public class CloudinaryService : ICloudinaryService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly Cloudinary _cloudinary;
+
+        public CloudinaryService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            
+            var account = new Account(
+                _configuration["Cloudinary:CloudName"],
+                _configuration["Cloudinary:ApiKey"],
+                _configuration["Cloudinary:ApiSecret"]);
+
+            _cloudinary = new Cloudinary(account);
+        }
+
+
+        public string Store(string file)
+        {
+            string sourcePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "temp", file);
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(sourcePath),
+                UniqueFilename = true,
+                Folder = "zenbil/uploads/"
+            };
+
+            var uploadResult = _cloudinary.Upload(uploadParams);
+
+            return uploadResult.PublicId;
+        }
+
+        public string BuildUrl(string publicId, string crop = "fill", int width = 150, int height = 150)
+        {
+            return _cloudinary.Api.Url.Secure(true)
+                .Transform(new Transformation().Width(width).Height(height).Crop(crop))
+                .BuildUrl(publicId);
+        }
+
+        public void Delete(string publicId)
+        {
+            _cloudinary.DeleteResources(publicId);
+        }
+    }
+}
